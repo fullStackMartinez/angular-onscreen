53e1885d8ea67d3ea4b9991afc852b5a44dd7a28
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-27 10:49:32 -0700

    added app.component.html and edited app.component.ts to look for app.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import {RouterModule, Routes} from "@angular/router";
-import {SplashComponent} from "./components/splash.component";
-import {UserService} from "./services/user.service";
-import {APP_BASE_HREF} from "@angular/common";
+import {SplashComponent} from "./splash/splash.component";
+import {UserService} from "./shared/services/user.service";
 
 
 export const allAppComponents = [SplashComponent];
@@ -11,7 +10,6 @@ export const routes: Routes = [
 ];
 
 export const appRoutingProviders: any[] = [
-	{provide: APP_BASE_HREF, useValue: window["_base_href"]},
 	UserService
 ];
 
